4c3f0a387900c4fdd3753dfc5f00fb34980881b4
JakubStasiak00 <125593509+JakubStasiak00@users.noreply.github.com>
2023-06-20 20:44:57 +0200

    card placement changes

--- src/pages/ShopPage.vue
+++ src/pages/ShopPage.vue
@@ -1,13 +1,15 @@
 <template>
     <h2 class="q-mx-auto q-px-lg text-h4 text-center text-bold" style="max-width: 1100px;"> Please explore our impressive range of state-of-the-art personal computers, showcasing the latest advancements in technology. We offer the convenience of allowing you to preview these exceptional products before making your in-store purchase. </h2>
     <div class="row justify-evenly q-ma-md q-gutter-md">
-        <q-card v-for="product in products" :key="product.id" class="q-pa-sm">
+        <q-card v-for="product in products" :key="product.id" class="q-pa-md">
+            <div class="text-h5 text-bold" style="max-width: 300px; min-height:95px;"> 
+                {{ product.name }}
+            </div>
+
             <q-img src="../images/dummy-pc.webp" v-if="product.rating === 5" />
             <q-img src="../images/dummy-mid.webp" v-else-if="product.rating === 4" />
             <q-img src="../images/dummy-worse.webp" v-else />
-            <div class="text-h5 text-bold" style="max-width: 300px;"> 
-                {{ product.name }}
-            </div>
+
             <div class="flex items-center">
                 <span class="text-subtitle1 text-bold">Performance Score:</span><q-rating class="q-pa-md" v-model="product.rating" max="5" size="sm" icon="build" readonly color="accent" />
             </div>
